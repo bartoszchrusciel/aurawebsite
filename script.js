34f57efcc9790a548a1f1236f519557d9164992a
Bartosz Chruściel <bartoszchrusciel@gmail.com>
2022-06-30 14:03:02 +0200

    configuration of nav

--- script.js
+++ script.js
@@ -26,23 +26,3 @@ function showSlides(n) {
     slides[numerSlajdu-1].style.display = "block";
     dots[numerSlajdu-1].className += " active";
 }
-
-
-function changeSlides() {
-  let i;
-  let slides = document.querySelectorAll(".slideImage");
-  let dots = document.querySelectorAll(".single-dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-}
-  numerSlajdu++;
-  if (numerSlajdu > slides.length) {numerSlajdu = 1}
-  slides[numerSlajdu-1].style.display = "block";
-  dots[numerSlajdu-1].className += " active";
-  setTimeout(changeSlides, 5000); // Change image every 2 seconds
-}
-
-changeSlides();
